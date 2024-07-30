488ec11b4f7c65dadd361be76dcd55637cf06232
yungwknd <yungwkndllc@gmail.com>
2024-07-30 10:46:09 -0700

    white walls
    Signed-off-by: yungwknd <yungwkndllc@gmail.com>

--- sketch.js
+++ sketch.js
@@ -1,5 +1,3 @@
-//// TO BE REMOVED ////
-
 function genTokenData(projectNum) {
   let data = {};
   let hash = "0x";
@@ -32,282 +30,67 @@ if (hash) {
 
 let R = new Random(tokenData);
 
-let X_WIDTH = 400;
-let Y_HEIGHT = 600;
+const totalSize = 1000;
+const borderSize = 125;
+let chosenPalette = murakamiPalette;
 
-const THANK_YOUS = [
-  "gracias ",
-  "thank you ",
-  "merci ",
-  "danke ",
-  "obrigado ",
-  "arigato ",
-  "shukran ",
-  "dhanyavaad ",
-  "spasibo ",
-  "grazie ",
-  "takk ",
-  "köszönöm ",
-  "toda ",
-  "tak ",
-  "kamsahamnida ",
-  "xie xie ",
-];
+let style = 1;
+let flowers = [];
 
-let flaps = [];
-
-let SIZE = 80;
-let v = [];
-let cols = 600,
-  rows = 10;
-
-let t_D = (180 * 15) / cols;
-let r_D = 1 / rows;
-
-let opening = 3,
-  vDensity = 8,
-  pAlign = 3.9,
-  curve1 = 2,
-  curve2 = 1.3;
-let opening_, vDensity_, pAlign_, curve1_, curve2_;
-
-let fonts = [];
-function preload() {
-  // fonts.push(loadFont("data/BritneyOnchainVF.ttf"));
-  fonts.push(loadFont("data/Savior1.ttf"));
-  fonts.push(loadFont("data/SpecialElite-Regular.ttf"));
-}
-
-let offscreenGraphics;
-
-let q = 2,
-  p = 8,
-  a = 100;
-let degree;
-
-let capturedImage;
-let textureBg;
-
-let hatch_brushes = ["marker"];
-let stroke_brushes = ["charcoal"];
-
-let bgImg;
-
-let twinTraits = {};
-// Get the "twin" mint
-const twinHash = urlParams.get("twin");
-if (twinHash) {
-  // Calculate the twin's traits
-  let twinData = tokenData;
-  twinData.hash = twinHash;
-
-  // Make an R for the twin data
-  let R_twin = new Random(twinData);
-
-  // Go through as if it is setup for twin..
-  twinTraits = {
-    bgPalette: [],
-    boxBg: "",
-  };
-
-  twinTraits.bgPalette = R_twin.random_choice(allPalettes);
-  if (R_twin.random_bool(90)) {
-    twinTraits.boxBg = R_twin.random_choice(twinTraits.bgPalette);
-  } else {
-    twinTraits.boxBg = "transparent";
-  }
-  twinTraits.offScreenStroke = R_twin.random_choice(["black", "white"]);
-  twinTraits.language = R_twin.random_choice(THANK_YOUS);
-  twinTraits.font = R_twin.random_choice(fonts);
-  twinTraits.fontSize = R_twin.random_int(30, 80);
-}
-
-let traits = {
-  bgPalette: twinHash ? twinTraits.bgPalette : [],
-  boxBg: "",
-};
+let bgColor;
 
 function setup() {
-  frameRate(10);
-  createCanvas(X_WIDTH, Y_HEIGHT, WEBGL);
-  offscreenGraphics = createGraphics(
-    windowWidth * 0.5,
-    (windowWidth * 0.5 * 1) / 1,
-    WEBGL
-  );
+  createCanvas(totalSize, totalSize);
+  background(255);
+  // noLoop(); // Stops draw from continuously looping
+  rect(0, 0, width, height);
+  chosenPalette = random(allPalettes);
 
-  // TRAITS //
-  if (!twinHash) {
-    traits.bgPalette = R.random_choice(allPalettes);
-  }
-  if (R.random_bool(90)) {
-    traits.boxBg = R.random_choice(traits.bgPalette);
-    offscreenGraphics.background(traits.boxBg);
-  } else {
-    traits.boxBg = "transparent";
-  }
-  traits.offScreenStroke = R.random_choice(["black", "white"]);
-  traits.language = R.random_choice(THANK_YOUS);
-  traits.font = R.random_choice(fonts);
-  traits.fontSize = R.random_int(30, 80);
+  style = R.random_choice([1, 2, 3]);
 
-  // TRAITS //
-
-  bgImage = createImage(X_WIDTH, Y_HEIGHT);
-
-  offscreenGraphics.angleMode(DEGREES);
-
-  offscreenGraphics.translate(
-    -offscreenGraphics.width / 2,
-    -offscreenGraphics.height / 2
-  );
-
-  offscreenGraphics.stroke(traits.offScreenStroke);
-  offscreenGraphics.noFill();
-
-  // offscreenGraphics.translate(width / 8, height / 8);
-
-  // Draw a border
-  // offscreenGraphics.rect(0, 0, (width / 8) * 6, (height / 8) * 6);
-
-  offscreenGraphics.fill(traits.offScreenStroke);
-  offscreenGraphics.textFont(traits.font);
-
-  const rows = Math.floor(offscreenGraphics.width / traits.fontSize);
-  const rowSize = offscreenGraphics.width / rows;
-  const colSize = offscreenGraphics.height / rows;
-
-  // Divide the border rect into a grid and draw a circle in the center of each cell
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < rows; j++) {
-      // Draw a rect in the center of the cell
-      offscreenGraphics.noFill();
-      // rect(0, 0, rowSize, colSize);
-      offscreenGraphics.textSize(traits.fontSize);
-      let letter = traits.language[(i * 5 + j) % traits.language.length];
-      offscreenGraphics.fill(traits.offScreenStroke);
-      offscreenGraphics.textAlign(LEFT, TOP);
-      offscreenGraphics.text(letter, rowSize / 6, 0);
-
-      offscreenGraphics.translate(rowSize, 0);
+  if (style === 2) {
+    for (let i = 0; i < 1000; i++) {
+      let x = R.random_num(0, width);
+      let y = R.random_num(0, height);
+      flower = new DrawCuteLittleFlower(x, y, 5, 0.1);
+      flowers.push(flower);
     }
-    offscreenGraphics.translate(-rowSize * rows, colSize);
+  }
+  if (style === 3) {
+    for (let i = 0; i < 500; i++) {
+      let x = R.random_num(0, width);
+      let y = R.random_num(0, height);
+      flower = new DrawCubicFlower(x, y, R.random_num(50, 100));
+      flowers.push(flower);
+    }
   }
 
-  offscreenGraphics.textSize(10);
-
-  brush.seed(tokenData.hash);
-
-  // Draw the background 10 times...
-  for (let i = 0; i < 20; i++) {
-    brush.noField();
-
-    brush.fill(R.random_choice(traits.bgPalette), R.random_int(60, 100));
-    brush.bleed(R.random_num(0.1, 0.4));
-    brush.fillTexture(0.55, 0.8);
-    brush.set(
-      R.random_choice(stroke_brushes),
-      R.random_choice(traits.bgPalette)
-    );
-    brush.setHatch(
-      R.random_choice(hatch_brushes),
-      R.random_choice(traits.bgPalette)
-    );
-    brush.hatch(R.random_int(10, 60), R.random_int(0, 180), {
-      rand: 0,
-      continuous: false,
-      gradient: false,
-    });
-    brush.rect(0, 0, width * 2, height * 2);
-  }
-
-  capturedImage = get();
-
-  console.log("Traits:", traits);
+  bgColor = R.random_choice(chosenPalette);
 }
 
 function draw() {
-  if (frameCount > 1) {
-    // noLoop();
-  }
-  image(capturedImage, -width / 2, -height / 2);
-  // orbitControl();
+  background(bgColor);
 
-  translate(0, 0, 200);
-  rotateX(frameCount * 0.03);
-  rotateY(frameCount * 0.03);
-  rotateZ(frameCount * 0.03);
-
-  // rotateX(-PI / 4);
-  texture(offscreenGraphics);
-
-  box(100);
-
-  texture(capturedImage);
-
-  translate(0, -5, 0);
-  box(10, 115, 115);
-  box(106, 10, 106);
-  box(115, 115, 10);
-
-  translate(0, -50, 0);
-  box(110, 10, 110);
-  translate(0, 10, 0);
-
-  scale(0.15);
-
-  noStroke();
-
-  for (let r = 0; r <= rows; r++) {
-    v.push([]);
-    for (let theta = 0; theta <= cols; theta++) {
-      let phi = (180 / opening) * Math.exp((-theta * t_D) / (vDensity * 180));
-      let petalCut =
-        1 -
-        (1 / 2) *
-          pow(
-            (5 / 4) * pow(1 - ((pAlign * theta * t_D) % 360) / 180, 2) - 1 / 4,
-            2
-          );
-      let hangDown =
-        curve1 * pow(r * r_D, 2) * pow(curve2 * r * r_D - 1, 2) * sin(phi);
-
-      let pX =
-        260 *
-        petalCut *
-        (r * r_D * sin(phi) + hangDown * cos(phi)) *
-        sin(theta * t_D);
-      let pY = -260 * petalCut * (r * r_D * cos(phi) - hangDown * sin(phi));
-      let pZ =
-        260 *
-        petalCut *
-        (r * r_D * sin(phi) + hangDown * cos(phi)) *
-        cos(theta * t_D);
-
-      let pos = createVector(pX, pY, pZ);
-      v[r].push(pos);
+  if (style === 1) {
+    for (let i = 0; i < 1000; i++) {
+      let x = R.random_num(0, width);
+      let y = R.random_num(0, height);
+      let size = R.random_num(30, 100);
+      drawMurakamiFlower(x, y, size);
+    }
+  } else if (style === 2) {
+    for (let flower of flowers) {
+      flower.draw();
+      flower.update();
+    }
+  } else if (style === 3) {
+    for (let flower of flowers) {
+      flower.drawFlower();
     }
   }
-  colorMode(HSB);
-  resetShader();
-  for (let r = 0; r < v.length; r++) {
-    fill(0, 0, -20 + r * r_D * 120, frameCount / 200);
-    for (let theta = 0; theta < v[r].length; theta++) {
-      if (r < v.length - 1 && theta < v[r].length - 1) {
-        beginShape();
-        vertex(v[r][theta].x, v[r][theta].y, v[r][theta].z);
-        vertex(v[r + 1][theta].x, v[r + 1][theta].y, v[r + 1][theta].z);
-        vertex(
-          v[r + 1][theta + 1].x,
-          v[r + 1][theta + 1].y,
-          v[r + 1][theta + 1].z
-        );
-        vertex(v[r][theta + 1].x, v[r][theta + 1].y, v[r][theta + 1].z);
-        endShape(CLOSE);
-      }
-    }
+  fill("white");
+  rect(borderSize, borderSize, width - 2 * borderSize, height - 2 * borderSize);
+  if (style === 1 || style === 3) {
+    noLoop();
   }
-
-  v = [];
-  colorMode(RGB);
 }
